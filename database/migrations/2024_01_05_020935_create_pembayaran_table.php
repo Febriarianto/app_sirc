523ec60d35428846421926a24683311fbbc91b3b
User ASUS <febriarianto464@gmail.com>
2024-02-10 21:49:54 +0700

    add: pembayaran lainnya

--- database/migrations/2024_01_05_020935_create_pembayaran_table.php
+++ database/migrations/2024_01_05_020935_create_pembayaran_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('pembayaran', function (Blueprint $table) {
             $table->id();
             $table->foreignId('id_transaksi')->references('id')->on('transaksi')->onUpdate('cascade')->onDelete('cascade');
-            $table->enum('tipe', ['dp', 'pelunasan']);
+            $table->enum('tipe', ['dp', 'pelunasan', 'titip', 'lainnya']);
             $table->unsignedBigInteger('nominal');
             $table->enum('metode', ['cash', 'transfer']);
             $table->string('file')->nullable();
